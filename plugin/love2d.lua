b80d2c7c5d4867232367cee793219dbbc00a3406
S1M0N38 <bertolottosimone@gmail.com>
2024-02-09 16:14:47 +0100

    feat: user command `LoveRun`
    this is the main command that the user should call.
    It search for the dir containing main.lua and call love2d.
    It autocomplete with dirs

---
+++ plugin/love2d.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_user_command("LoveRun", function(args)
+  local path = args.args
+  local main = ""
+  if path == "" then
+    main = vim.fn.findfile("main.lua", ".;")
+    path = vim.fn.fnamemodify(main, ":h")
+  else
+    main = vim.fn.findfile("main.lua", path)
+  end
+  if main == "" then
+    vim.notify("No main.lua file found", vim.log.levels.ERROR)
+    return
+  else
+    ---@cast path string
+    require("love2d").run(path)
+  end
+end, { nargs = "?", complete = "dir" })
--- plugin/my_awesome_plugin.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_user_command("PluginName", function(args)
-  local a, b, func = unpack(args.fargs)
-  a = tonumber(a)
-  b = tonumber(b)
-  assert(a, "a must be a number")
-  assert(b, "b must be a number")
-  require("my_awesome_plugin").main(a, b, func)
-end, { nargs = "+" })
