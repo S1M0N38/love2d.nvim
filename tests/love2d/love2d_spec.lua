fb2fa88e9154c4ef04ce49a222a86beebf5d358e
S1M0N38 <bertolottosimone@gmail.com>
2024-02-10 17:26:18 +0100

    test: support test on multiple platforms (macos and linux)

--- tests/love2d/love2d_spec.lua
+++ tests/love2d/love2d_spec.lua
@@ -1,22 +1,40 @@
 local love2d = require("love2d")
 
-describe("love2d mac", function()
+local opts
+if vim.fn.has("mac") == 1 then
+  opts = {
+    path_to_love = "/Applications/love.app/Contents/MacOS/love",
+    path_to_love_library = "",
+  }
+elseif vim.fn.has("linux") == 1 then
+  opts = {
+    path_to_love = "/usr/bin/love",
+    path_to_love_library = "",
+  }
+else
+  error("OS not supported")
+end
+
+describe("love2d platform", function()
   it("does not start with wrong path_to_love", function()
-    love2d.setup({ path_to_love = "/Applications/love.app/Contents/MacOS/foo" })
+    love2d.setup({
+      path_to_love = "this/path/does/not/exist/love",
+      path_to_love_library = "",
+    })
     love2d.run("tests/game")
     vim.wait(1000)
     assert.equal(nil, love2d.job.id)
     vim.wait(500)
   end)
   it("starts", function()
-    love2d.setup({ path_to_love = "/Applications/love.app/Contents/MacOS/love" })
+    love2d.setup(opts)
     love2d.run("tests/game")
     vim.wait(1000)
     assert.equal(1, vim.fn.jobstop(love2d.job.id))
     vim.wait(500)
   end)
   it("does not run foo with wrong path to game", function()
-    love2d.setup({ path_to_love = "/Applications/love.app/Contents/MacOS/love" })
+    love2d.setup(opts)
     love2d.run("tests/foo")
     vim.wait(1000)
     assert.equal(1, vim.fn.jobstop(love2d.job.id))
@@ -24,7 +42,7 @@ describe("love2d mac", function()
     assert.equal(1, love2d.job.exit_code)
   end)
   it("runs game", function()
-    love2d.setup({ path_to_love = "/Applications/love.app/Contents/MacOS/love" })
+    love2d.setup(opts)
     love2d.run("tests/game")
     vim.wait(1000)
     assert.equal(1, vim.fn.jobstop(love2d.job.id))
@@ -32,7 +50,7 @@ describe("love2d mac", function()
     assert.equal(0, love2d.job.exit_code)
   end)
   it("stops", function()
-    love2d.setup({ path_to_love = "/Applications/love.app/Contents/MacOS/love" })
+    love2d.setup(opts)
     love2d.run("tests/game")
     vim.wait(1000)
     love2d.stop()
