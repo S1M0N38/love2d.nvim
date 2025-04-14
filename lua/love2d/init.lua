17b60a1fa5c775a7d2d8436a5acb54e73254b7f4
deadYokai <mynameiskitsune@proton.me>
2025-04-15 01:31:36 +0300

    Update

--- lua/love2d/init.lua
+++ lua/love2d/init.lua
@@ -8,108 +8,109 @@ local love2d = {}
 ---@param path string: The path to the Love2D project. If "" search for it.
 ---@return string?: The path to the Love2D project. nil if not found
 love2d.find_src_path = function(path)
-  local main
-  if path == "" then
-    main = vim.fn.findfile("main.lua", ".;")
-  else
-    main = vim.fn.findfile("main.lua", path)
-  end
-  if main == "" then
-    return
-  end
-  return vim.fn.fnamemodify(main, ":h")
+	local main
+	if path == "" then
+		main = vim.fn.findfile("main.lua", ".;")
+	else
+		main = vim.fn.findfile("main.lua", path)
+	end
+	if main == "" then
+		return
+	end
+	return vim.fn.fnamemodify(main, ":h")
 end
 
 ---Initialize Love2D with options
 ---@param opts options: The options to initialize Love2D with
 love2d.setup = function(opts)
-  require("love2d.config").setup(opts)
+	require("love2d.config").setup(opts)
 end
 
 ---Run a Love2D project
 ---@param path string: The path to the Love2D project
 love2d.run = function(path)
-  if love2d.job and love2d.job.id then
-    vim.notify("A LÖVE project is already running.", vim.log.levels.WARN)
-    return
-  end
-  love2d.job = {} -- reset job
-  vim.notify("Running LÖVE project at " .. path)
-  local cmd = require("love2d.config").options.path_to_love_bin .. " " .. path
-	local enable_nvim_term = require("love2d.config").options.nvim_term_buff
+	if love2d.job and love2d.job.id then
+		vim.notify("A LÖVE project is already running.", vim.log.levels.WARN)
+		return
+	end
+	love2d.job = {} -- reset job
+	vim.notify("Running LÖVE project at " .. path)
+	local cmd = require("love2d.config").options.path_to_love_bin .. " " .. path
 
 	local _o = {
-    on_exit = function(_, code)
-      love2d.job.exit_code = code
-      love2d.job.id = nil
-    end,
-  }
+		on_exit = function(_, code)
+			love2d.job.exit_code = code
+			love2d.job.id = nil
+		end,
+	}
 
-	if enable_nvim_term then
+	if require("love2d.config").options.window_output then
+
+		if not love2d.job.augroup then
+			vim.api.nvim_create_augroup('Love2D', {})
+		end
 
 		if not love2d.job.buf then
-			love2d.job.buf = vim.api.nvim_create_buf(true, true)
+			love2d.job.buf = vim.api.nvim_create_buf(false, true)
 
+			if not love2d.job.win then
+				love2d.job.win = vim.api.nvim_open_win(love2d.job.buf, false,	{
+					split = "below",
+				})
 
-			love2d.job.win = vim.api.nvim_open_win(love2d.job.buf, false,	{
-				split = "below",
-			})
-
-			love2d.job.chan = vim.api.nvim_open_term(love2d.job.buf, {})
-		end
-
-		vim.api.nvim_create_autocmd("BufWipeout", {
-			buffer = love2d.job.buf,
-			callback = function()
-				if love2d.job.id then
-					vim.fn.jobstop(love2d.job.id)
-				end
-				love2d.job.id = nil
-				love2d.job.chan = nil
-				love2d.job.buf = nil
-				love2d.job.win = nil
+				vim.api.nvim_create_autocmd('WinClosed', {
+					group = love2d.job.augroup,
+					callback = function(args)
+						if args.match == tostring(love2d.job.win) then
+							vim.api.nvim_buf_delete(love2d.job.buf, { force = true })
+							love2d.job.win = nil
+							love2d.job.buf = nil
+							return true
+						end
+					end
+				})
 			end
-		})
 
-
-		vim.bo[love2d.job.buf].filetype = 'terminal'
-		if vim.treesitter then
-			vim.treesitter.stop(love2d.job.buf)
+			vim.api.nvim_win_set_buf(love2d.job.win, love2d.job.buf)
+			vim.bo[love2d.job.buf].buftype = "nofile"
+			vim.bo[love2d.job.buf].bufhidden = "wipe"
+			vim.bo[love2d.job.buf].swapfile = false
+			vim.wo[love2d.job.win].number = false
+			vim.wo[love2d.job.win].relativenumber = false
 		end
-		vim.api.nvim_clear_autocmds({ buffer = love2d.job.buf })
 
-		_o.pty = true
+
 		_o.on_stdout = function(_, data)
-			if love2d.job.chan ~= nil and love2d.job.buf and vim.api.nvim_buf_is_valid(love2d.job.buf) and vim.api.nvim_buf_is_loaded(love2d.job.buf) then
-				pcall(vim.api.nvim_chan_send, love2d.job.chan, table.concat(data, "\n"))
+
+			if love2d.job.buf and vim.api.nvim_buf_is_valid(love2d.job.buf) and vim.api.nvim_buf_is_loaded(love2d.job.buf) then
+
+				local lines = vim.tbl_filter(function(line) return line ~= "" end, data)
+				vim.api.nvim_buf_set_lines(love2d.job.buf, -1, -1, false, lines)
 			end
+
 		end
+
 		_o.on_exit = function(_, code)
-			if love2d.job.chan ~= nil and love2d.job.buf and vim.api.nvim_buf_is_valid(love2d.job.buf) and vim.api.nvim_buf_is_loaded(love2d.job.buf) then
-				pcall(vim.api.nvim_chan_send, love2d.job.chan, '\r\n[Process exited with code: ' .. tostring(code) .. ']')
-			end
 			love2d.job.exit_code = code
 			if love2d.job.id then
 				vim.fn.jobstop(love2d.job.id)
 			end
 			love2d.job.id = nil
-			love2d.job.chan = nil
-			love2d.job.buf = nil
-			love2d.job.win = nil
 		end
+
 	end
 
-  love2d.job.id = vim.fn.jobstart(cmd, _o)
+	love2d.job.id = vim.fn.jobstart(cmd, _o)
 end
 
 ---Stop the running project
 love2d.stop = function()
-  if not love2d.job or not love2d.job.id then
-    vim.notify("No LÖVE project running.", vim.log.levels.WARN)
-    return
-  end
-  vim.notify("Stop LÖVE project")
-  vim.fn.jobstop(love2d.job.id)
+	if not love2d.job or not love2d.job.id then
+		vim.notify("No LÖVE project running.", vim.log.levels.WARN)
+		return
+	end
+	vim.notify("Stop LÖVE project")
+	vim.fn.jobstop(love2d.job.id)
 end
 
 return love2d
