df5581231d3982caef162d4dc48e03c65f80e6cc
S1M0N38 <bertolottosimone@gmail.com>
2025-04-15 15:58:24 +0200

    style: move enable debug win in a separate function

--- lua/love2d/init.lua
+++ lua/love2d/init.lua
@@ -4,6 +4,61 @@ local love2d = {}
 ---@field id number: job-id returned by vim.fn.jobstart
 ---@field exit_code number: exit-code intercepted by on_exit callback
 
+---Options and Initializations for debug window
+---@param job_opts table
+---@param window_opts table
+local function enable_debug_window(job_opts, window_opts)
+  if not love2d.job.augroup then
+    vim.api.nvim_create_augroup("Love2D", {})
+  end
+
+  if not love2d.job.buf then
+    love2d.job.buf = vim.api.nvim_create_buf(false, true)
+
+    if not love2d.job.win then
+      love2d.job.win = vim.api.nvim_open_win(love2d.job.buf, false, window_opts)
+
+      vim.api.nvim_create_autocmd("WinClosed", {
+        group = love2d.job.augroup,
+        callback = function(args)
+          if args.match == tostring(love2d.job.win) then
+            vim.api.nvim_buf_delete(love2d.job.buf, { force = true })
+            love2d.job.win = nil
+            love2d.job.buf = nil
+            return true
+          end
+        end,
+      })
+    end
+
+    vim.api.nvim_win_set_buf(love2d.job.win, love2d.job.buf)
+    vim.bo[love2d.job.buf].buftype = "nofile"
+    vim.bo[love2d.job.buf].bufhidden = "wipe"
+    vim.bo[love2d.job.buf].swapfile = false
+    vim.wo[love2d.job.win].number = false
+    vim.wo[love2d.job.win].relativenumber = false
+  end
+
+  job_opts.on_stdout = function(_, data)
+    if love2d.job.buf and vim.api.nvim_buf_is_valid(love2d.job.buf) and vim.api.nvim_buf_is_loaded(love2d.job.buf) then
+      local lines = vim.tbl_filter(function(line)
+        return line ~= ""
+      end, data)
+      vim.api.nvim_buf_set_lines(love2d.job.buf, -1, -1, false, lines)
+    end
+  end
+
+  job_opts.on_exit = function(_, code)
+    love2d.job.exit_code = code
+    if love2d.job.id then
+      vim.fn.jobstop(love2d.job.id)
+    end
+    love2d.job.id = nil
+  end
+
+  return job_opts
+end
+
 ---Find a valid path to the Love2D project
 ---@param path string: The path to the Love2D project. If "" search for it.
 ---@return string?: The path to the Love2D project. nil if not found
@@ -37,70 +92,19 @@ love2d.run = function(path)
   vim.notify("Running LÖVE project at " .. path)
   local cmd = require("love2d.config").options.path_to_love_bin .. " " .. path
 
-  local _o = {
+  local job_opts = {
     on_exit = function(_, code)
       love2d.job.exit_code = code
       love2d.job.id = nil
     end,
   }
 
-  if require("love2d.config").options.window_output then
-    if not love2d.job.augroup then
-      vim.api.nvim_create_augroup("Love2D", {})
-    end
-
-    if not love2d.job.buf then
-      love2d.job.buf = vim.api.nvim_create_buf(false, true)
-
-      if not love2d.job.win then
-        love2d.job.win = vim.api.nvim_open_win(love2d.job.buf, false, {
-          split = "below",
-        })
-
-        vim.api.nvim_create_autocmd("WinClosed", {
-          group = love2d.job.augroup,
-          callback = function(args)
-            if args.match == tostring(love2d.job.win) then
-              vim.api.nvim_buf_delete(love2d.job.buf, { force = true })
-              love2d.job.win = nil
-              love2d.job.buf = nil
-              return true
-            end
-          end,
-        })
-      end
-
-      vim.api.nvim_win_set_buf(love2d.job.win, love2d.job.buf)
-      vim.bo[love2d.job.buf].buftype = "nofile"
-      vim.bo[love2d.job.buf].bufhidden = "wipe"
-      vim.bo[love2d.job.buf].swapfile = false
-      vim.wo[love2d.job.win].number = false
-      vim.wo[love2d.job.win].relativenumber = false
-    end
-
-    _o.on_stdout = function(_, data)
-      if
-        love2d.job.buf
-        and vim.api.nvim_buf_is_valid(love2d.job.buf)
-        and vim.api.nvim_buf_is_loaded(love2d.job.buf)
-      then
-        local lines = vim.tbl_filter(function(line)
-          return line ~= ""
-        end, data)
-        vim.api.nvim_buf_set_lines(love2d.job.buf, -1, -1, false, lines)
-      end
-    end
-
-    _o.on_exit = function(_, code)
-      love2d.job.exit_code = code
-      if love2d.job.id then
-        vim.fn.jobstop(love2d.job.id)
-      end
-      love2d.job.id = nil
-    end
+  local window_opts = require("love2d.config").options.debug_window_opts
+  if window_opts then
+    job_opts = enable_debug_window(job_opts, window_opts)
   end
 
-  love2d.job.id = vim.fn.jobstart(cmd, _o)
+  love2d.job.id = vim.fn.jobstart(cmd, job_opts)
 end
 
 ---Stop the running project
