efe9b6947766352986ac725d050ca85b0d41cbc7
S1M0N38 <bertolottosimone@gmail.com>
2026-05-11 14:33:37 +0200

    refactor(plugin): replace LoveRun/LoveStop with :Love subcommand
    Replace separate LoveRun/LoveStop commands with a single :Love command
    supporting run, watch, stop, info, and output subcommands with
    fuzzy completion.

--- plugin/love2d.lua
+++ plugin/love2d.lua
@@ -1,14 +1,39 @@
-vim.api.nvim_create_user_command("LoveRun", function(args)
-  local love2d = require("love2d")
-  local path = love2d.find_src_path(args.args)
-  if path then
-    love2d.run(path)
-  else
-    vim.notify("No main.lua file found", vim.log.levels.ERROR)
-  end
-end, { nargs = "?", complete = "dir" })
+local sub_cmds = {
+  run = function()
+    require("love2d.job").run()
+  end,
+  watch = function()
+    require("love2d.job").watch()
+  end,
+  stop = function()
+    require("love2d.job").stop()
+  end,
+  info = function()
+    require("love2d.job").info()
+  end,
+  output = function()
+    require("love2d.output").toggle()
+  end,
+}
 
-vim.api.nvim_create_user_command("LoveStop", function()
-  local love2d = require("love2d")
-  love2d.stop()
-end, {})
+local sub_cmds_keys = vim.tbl_keys(sub_cmds)
+
+vim.api.nvim_create_user_command("Love", function(opts)
+  local sub_cmd = sub_cmds[opts.args]
+  if sub_cmd == nil then
+    vim.notify("Love: invalid subcommand", vim.log.levels.ERROR, { title = "love2d" })
+  else
+    sub_cmd()
+  end
+end, {
+  nargs = "?",
+  desc = "LÖVE game runner",
+  complete = function(arg_lead, _, _)
+    return vim
+      .iter(sub_cmds_keys)
+      :filter(function(sub_cmd)
+        return sub_cmd:find(arg_lead) ~= nil
+      end)
+      :totable()
+  end,
+})
